c1aa89efcdc3685c82567f6419a2d89414a98a9e
Subado <redcatgamer1980@gmail.com>
2022-11-17 01:20:20 +0400

    Fix background and foreground colors

--- config.def.h
+++ config.def.h
@@ -135,10 +135,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 256;
 
 /*
  * Default shape of cursor
